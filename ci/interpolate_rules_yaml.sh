11310409230069a143b51e5a3aea789cc3f06447
Ardiea <mas48@mit.edu>
2022-04-21 15:15:08 -0400

    Fixing file name.

---
+++ ci/interpolate_rules_yaml.sh
@@ -0,0 +1,13 @@
+#
+# These need to be set for the various sed statements. If you have new subsitutions to add, here is where
+# you would define them but if they require some secret piece of information you'll need to update the pipeline
+# definition in ol-infrastructure to make the vars available.
+# 
+# RESOURCE_NAME : the name (directory) where this repo can be found within the context of the concourse pipeline 
+# RULE_DIRECTORY : What kind of rules are we syncing (cortex-rules or loki-rules)
+# ENVIRONMENT_NAME : The environment name (ci, qa, production).
+# 
+
+echo "executing sed statements for secret interpolation/etc"
+sed -i -e 's/%% ENVIRONMENT_NAME %%/$ENVRIONMENT_NAME/' $RESOURCE_NAME/$RULE_DIRECTORY/*
+echo "executing cortextool"
