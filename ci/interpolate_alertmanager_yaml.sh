1d0edff2248fe9a38166893b7c3379a9a0cc2e4e
Ardiea <mas48@mit.edu>
2025-05-09 12:50:41 -0400

    testing 2

--- ci/interpolate_alertmanager_yaml.sh
+++ ci/interpolate_alertmanager_yaml.sh
@@ -13,6 +13,6 @@ echo "executing sed statements for secret interpolation/etc"
 sed -i -e "s/%% OPS_TEAM_OPS_GENIE_API_KEY %%/$OPS_TEAM_OPS_GENIE_API_KEY/" $RESOURCE_NAME/alertmanager.yaml
 sed -i -e "s/%% TESTING_OPS_GENIE_API_KEY %%/$TESTING_OPS_GENIE_API_KEY/" $RESOURCE_NAME/alertmanager.yaml
 sed -i -e "s/%% ENVIRONMENT_NAME %%/$ENVIRONMENT_NAME/" $RESOURCE_NAME/alertmanager.yaml
-sed -i -e "s/%% SLACK_NOTIFICATIONS_OCW_MISC_API_URL %%/$SLACK_NOTIFICATIONS_OCW_MISC_API_URL/" $RESOURCE_NAME/alertmanager.yaml
 sed -i -e "s/%% ROOTLY_BEARER_TOKEN %%/$ROOTLY_BEARER_TOKEN/" $RESOURCE_NAME/alertmanager.yaml
+sed -i -e "s/%% SLACK_NOTIFICATIONS_OCW_MISC_API_URL %%/$SLACK_NOTIFICATIONS_OCW_MISC_API_URL/" $RESOURCE_NAME/alertmanager.yaml
 echo "executing cortextool"
